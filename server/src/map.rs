7eec99c51946a0f2cef1550130b9bee6f6579e06
Peter Nelson <peter@peterdn.com>
2020-06-27 12:32:52 +0100

    Add StartGame service method

---
+++ server/src/map.rs
@@ -0,0 +1,4 @@
+pub const MAP_WIDTH_MIN: i32 = 10;
+pub const MAP_WIDTH_MAX: i32 = 100;
+pub const MAP_HEIGHT_MIN: i32 = 10;
+pub const MAP_HEIGHT_MAX: i32 = 100;
--- server/src/server.rs
+++ server/src/server.rs
@@ -1,7 +1,9 @@
-use tonic::{transport::Server, Request, Response, Status};
+use tonic::{transport::Server, Code, Request, Response, Status};
 
 use game::game_server::{Game, GameServer};
-use game::{PlayGameRequest, PlayGameResponse};
+use game::{Map, PlayGameRequest, PlayGameResponse, Size, StartGameRequest, StartGameResponse};
+
+pub mod map;
 
 pub mod game {
     tonic::include_proto!("game");
@@ -12,6 +14,45 @@ struct TestGame {}
 
 #[tonic::async_trait]
 impl Game for TestGame {
+    async fn start_game(
+        &self,
+        request: Request<StartGameRequest>,
+    ) -> Result<Response<StartGameResponse>, Status> {
+        let request = request.into_inner();
+
+        if request.world_size.is_none() {
+            return Err(Status::new(
+                Code::InvalidArgument,
+                "No world size specified",
+            ));
+        }
+
+        let world_size = request.world_size.unwrap();
+
+        if world_size.width < map::MAP_WIDTH_MIN
+            || world_size.width > map::MAP_WIDTH_MAX
+            || world_size.height > map::MAP_HEIGHT_MAX
+            || world_size.height < map::MAP_HEIGHT_MIN
+        {
+            return Err(Status::new(Code::InvalidArgument, "Bad world size"));
+        }
+
+        let ncells = (world_size.width * world_size.height) as usize;
+        let cells = vec![0; ncells];
+
+        let map = game::Map {
+            map_size: Some(world_size),
+            cells,
+        };
+
+        let reply = game::StartGameResponse {
+            game_id: 100,
+            world_map: Some(map),
+        };
+
+        Ok(Response::new(reply))
+    }
+
     async fn play_game(
         &self,
         request: Request<PlayGameRequest>,
